fb8e34bd853a1afa3391930b0cc4c061e54089dc
samoalex <104589079+samoalex@users.noreply.github.com>
2022-12-22 11:02:47 -0600

    Version1

--- sample_script.py
+++ sample_script.py
@@ -11,7 +11,7 @@ driver.get('https://www.google.com/')
 
 search = driver.find_element(By.NAME, 'q')
 search.clear()
-search.send_keys('Dress')
+search.send_keys('smar twath')
 
 # wait for 4 sec
 sleep(4)
@@ -20,7 +20,7 @@ sleep(4)
 driver.find_element(By.NAME, 'btnK').click()
 
 # verify
-assert 'dress' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
+assert 'smar twath' in driver.current_url.lower(), f"Expected query not in {driver.current_url.lower()}"
 print('Test Passed')
 
 driver.quit()
